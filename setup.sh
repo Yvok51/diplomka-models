363df4b751af8af4cfdcf48dcfbc3157793aff0e
Michal Tichý <michal.tichy.y@email.cz>
2025-05-12 20:32:33 +0200

    Add lang2vec

---
+++ setup.sh
@@ -0,0 +1,10 @@
+
+python3 -m venv venv
+venv/bin/pip install -r requirements.txt
+venv/bin/pip install -r requirements-pytorch.txt
+
+SITE_PACKAGES=$(venv/bin/python3 -c "import site; print(site.getsitepackages()[0])")
+LV_LIB="${SITE_PACKAGES}/lang2vec/data"
+LV_DISTANCES="${LV_LIB}/distances.zip"
+curl http://www.cs.cmu.edu/~aanastas/files/distances.zip --output "${LV_DISTANCES}"
+unzip "${LV_DISTANCES}" -d "${LV_LIB}"
