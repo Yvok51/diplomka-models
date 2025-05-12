b5c0afe300c6ef0d22d42881186c73fc3cc45ca7
Michal Tichý <michal.tichy.y@email.cz>
2025-05-12 08:51:09 +0200

    Modify job script

--- job.sh
+++ job.sh
@@ -1,10 +1,11 @@
  #!/bin/bash
 #PBS -N batch_job_example
-#PBS -l select=1:ncpus=8:mem=256gb:scratch_local=10gb
-#PBS -l walltime=16:00:00
+#PBS -l select=1:ncpus=8:ngpus=1:mem=256gb:scratch_local=10gb:gpu_mem=17gb
+#PBS -l walltime=240:00:00
 
 # define a DATADIR variable: directory where the input files are taken from and where the output will be copied to
-DATADIR=/storage/brno2/home/michal-tichy/diplomka/diplomka-models # substitute username and path to your real username and path
+MYHOME=/storage/brno2/home/michal-tichy
+DATADIR="${HOME}/diplomka/diplomka-models" # substitute username and path to your real username and path
 
 # append a line to a file "jobs_info.txt" containing the ID of the job, the hostname of the node it is run on, and the path to a scratch directory
 # this information helps to find a scratch directory in case the job fails, and you need to remove the scratch directory manually
@@ -12,11 +13,12 @@ echo "$PBS_JOBID is running on node `hostname -f` in a scratch directory $SCRATC
 
 module add python/python-3.10.4-intel-19.0.4-sc7snnf
 
-export HOME=$DATADIR
+export HOME=$MYHOME
+export TMPDIR=$SCRATCHDIR
 cd $DATADIR
 
 git pull
 venv/bin/pip install -r requirements.txt
 venv/bin/pip install -r requirements-pytorch.txt
 
-venv/bin/python3 src/multilabel --epochs 2 --samples-per-language 10000 --batch-size 96
+venv/bin/python3 src/multiclass.py --epochs 1 --batch-size 128
