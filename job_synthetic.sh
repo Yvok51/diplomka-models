8432f62190df1fa2f87714b888bc7cb2a41747f2
Michal Tichý <michal.tichy.y@email.cz>
2025-05-18 22:23:16 +0200

    Add synthetic job

---
+++ job_synthetic.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#PBS -N multilabel_synthetic_langID
+#PBS -l select=1:ncpus=8:ngpus=1:mem=256gb:scratch_local=10gb:gpu_mem=16gb
+#PBS -l walltime=16:00:00
+
+# define a DATADIR variable: directory where the input files are taken from and where the output will be copied to
+MYHOME=/storage/brno2/home/michal-tichy
+DATADIR="${HOME}/diplomka/diplomka-models" # substitute username and path to your real username and path
+
+# append a line to a file "jobs_info.txt" containing the ID of the job, the hostname of the node it is run on, and the path to a scratch directory
+# this information helps to find a scratch directory in case the job fails, and you need to remove the scratch directory manually
+echo "$PBS_JOBID is running on node `hostname -f` in a scratch directory $SCRATCHDIR" >> $DATADIR/jobs_info.txt
+
+module add python/python-3.10.4-intel-19.0.4-sc7snnf
+
+export HOME="${MYHOME}"
+export TMPDIR="${SCRATCHDIR}"
+cd $DATADIR
+
+git pull
+
+venv/bin/python3 src/multilabel.py --samples-per-language 20000 --model-path "multilabel_synthetic_${1}_output" --synthetic-proportion "${1}"
+
