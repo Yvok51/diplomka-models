8bfdfbc5d55e9395d8de0e3757c052b2cc01b609
Michal Tichý <michal.tichy.y@email.cz>
2025-06-05 20:53:21 +0200

    Alter length size in long multilabel job

--- job_long_multilabel.sh
+++ job_long_multilabel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N multilabel_langID
 #PBS -l select=1:ncpus=4:ngpus=1:mem=512gb:scratch_local=10gb:gpu_mem=17gb
-#PBS -l walltime=400:00:00
+#PBS -l walltime=300:00:00
 
 # define a DATADIR variable: directory where the input files are taken from and where the output will be copied to
 MYHOME=/storage/brno2/home/michal-tichy
