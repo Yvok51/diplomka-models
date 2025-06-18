dd0c7e1a38623b8fe3b6378966b3fea24515c752
Michal Tichý <michal.tichy.y@email.cz>
2025-06-18 09:07:33 +0200

    Decrease batch size aic scripts

--- aic_scripts/job_long_multilabel.sh
+++ aic_scripts/job_long_multilabel.sh
@@ -7,6 +7,6 @@
 #SBATCH -o job_long_multilabel.out
 #SBATCH -e job_long_multilabel.err
 
-venv/bin/python3 src/multilabel.py --epochs 1 --batch-size 128 --model-path long_multilabel_output --synthetic-proportion 0.75
+venv/bin/python3 src/multilabel.py --epochs 1 --batch-size 64 --model-path long_multilabel_output --synthetic-proportion 0.5
 mkdir results
 venv/bin/python3 src/flores_evaluation.py --model-path long_multilabel_output --type multilabel --encoder-path trainer_output/multilabel_encoder.pkl --output "results/flores_multilabel_long.txt"
