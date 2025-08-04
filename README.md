ad143fbf37cf15b939888a49f2e3d3430dbe5bcf
Michal Tichý <michal.tichy.y@email.cz>
2025-08-04 21:48:47 +0200

    Update config files

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - metacentrum qsub command: `qsub -q gpu -l select=1:ncpus=4:ngpus=1:mem=128gb:scratch_local=16gb:gpu_mem=16gb -I`
 - list of FLORES labels: [link](https://github.com/facebookresearch/flores/tree/main/flores200#languages-in-flores-200)
+- pycld3 package requires `protoc` - [The protocol buffers compiler](https://github.com/protocolbuffers/protobuf)
--- install.sh
+++ install.sh
@@ -2,5 +2,9 @@
 
 
 SITE_PACKAGES=$(venv/bin/python3 -c "import site; print(site.getsitepackages()[0])")
+
 LV_LIB="${SITE_PACKAGES}/lang2vec/lang2vec.py"
-sed -i 's/feature_database = np.load(filename, encoding="latin1").item()/feature_database = np.load(filename, encoding="latin1", allow_pickle=True).item()/g' "${LV_LIB}"
+sed -i 's/feature_database = np.load(filename, encoding="latin1").item()/feature_database = np.load(filename, encoding="latin1", allow_pickle=True).item()/g' $LV_LIB
+
+FASTTEXT_LIB="${SITE_PACKAGES}/fasttext/FastText.py"
+sed -i 's/np.array(probs, copy=False)/np.asarray(probs)/g' $FASTTEXT_LIB
