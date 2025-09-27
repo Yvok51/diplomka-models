732efc3eb7d5a2681121c7cb30572044e01cb81e
Michal Tichý <michal.tichy.y@email.cz>
2025-09-27 22:00:14 +0200

    Add multilabel script

---
+++ metacentrum_scripts/job_multilabel.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#PBS -N negative_langID
+#PBS -l select=1:ncpus=2:ngpus=1:mem=64gb:scratch_local=10gb:gpu_mem=15gb
+#PBS -l walltime=20:00:00
+
+# define a DATADIR variable: directory where the input files are taken from and where the output will be copied to
+MYHOME=/storage/brno2/home/michal-tichy
+DATADIR="${MYHOME}/diplomka/diplomka-models" # substitute username and path to your real username and path
+
+# append a line to a file "jobs_info.txt" containing the ID of the job, the hostname of the node it is run on, and the path to a scratch directory
+# this information helps to find a scratch directory in case the job fails, and you need to remove the scratch directory manually
+echo "$PBS_JOBID is running on node `hostname -f` in a scratch directory $SCRATCHDIR" >> $DATADIR/jobs_info.txt
+
+module add python/python-3.10.4-intel-19.0.4-sc7snnf
+
+export HOME=$MYHOME
+export TMPDIR=$SCRATCHDIR
+cd $DATADIR
+
+echo "HOME directory: ${HOME}"
+
+git pull
+
+MODEL_PATH="multilabel_${PORTION}_${TYPE}_output"
+venv/bin/python3 src/multilabel.py --samples-per-language 20000 --model-path "${MODEL_PATH}" --batch-size 16 --synthetic-proportion "${PORTION}" --model-type "${TYPE}"
+venv/bin/python3 src/flores_evaluation.py --model-path "${MODEL_PATH}" --type multilabel --encoder-path trainer_output/multilabel_encoder.pkl --output "results/${MODEL_PATH}.txt" --confusion-matrix
+
+
