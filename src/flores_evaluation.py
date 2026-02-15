0c2e38cd7c8dbf349fda5e84ee11c751583b7335
Michal Tichý <michal.tichy.y@email.cz>
2026-02-15 20:54:35 +0100

    Add script calls

--- src/flores_evaluation.py
+++ src/flores_evaluation.py
@@ -88,7 +88,15 @@ def false_positive_rate(predictions, gold, get_rates):
 
     return FP / (FP + TN)
 
-
+# Called:
+#   gcld3: python3 ./src/our_evaluation.py --input ../dataset/dataset.json --type gcld3 --output our_evaluation/gcld3.txt
+#   fasttext: python3 ./src/our_evaluation.py --input ../dataset/dataset.json --type fasttext --output our_evaluation/fasttext.txt
+#   openlid: python3 ./src/our_evaluation.py --input ../dataset/dataset.json --type openlid --output our_evaluation/openlid.txt
+#   glotlid: python3 ./src/our_evaluation.py --input ../dataset/dataset.json --type glotlid --output our_evaluation/glotlid.txt
+#   tf-idf multilabel: python3 ./src/our_evaluation.py --input ../dataset/dataset.json --model-path models/nli_multilabel_model_20260214_044828.pkl --encoder trainer_output/multilabel_encoder.pkl --type multilabel --output our_evaluation/tf_idf_multilabel.txt --model-kind tfidf
+#   tf-idf multiclass: python3 ./src/our_evaluation.py --input ../dataset/dataset.json --model-path models/nli_model_20260206_103101.pkl --encoder trainer_output/label_encoder.pkl --model-type canine --type multiclass --output our_evaluation/tf_idf_multiclass.txt --model-kind tfidf
+#   tf-idf multilabel: python3 ./src/our_evaluation.py --input ../dataset/dataset.json --model-path finished_multilabel/ --encoder trainer_output/multilabel_encoder.pkl --model-type canine --output our_evaluation/canine_multilabel.txt
+#   canine multiclass: python3 ./src/our_evaluation.py --input ../dataset/dataset.json --model-path finished_multiclass/ --encoder trainer_output/label_encoder.pkl --model-type canine --output our_evaluation/canine_multiclass.txt
 def main():
     parser = argparse.ArgumentParser(
         description="Evaluation of language prediction using finetuned CANINE model")
